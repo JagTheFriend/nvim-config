a97b44762141b4e53dbbf26c875e6f8142f81d45
JagTheFriend <JagTheFriend12@gmail.com>
2026-03-29 22:34:52 +0530

    Setup custom mapping for easier write operation

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,9 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+vim.api.nvim_create_user_command("W", "w", { desc = "Write file" })
+vim.api.nvim_create_user_command("Wa", "wa", { desc = "Write all files" })
+vim.api.nvim_create_user_command("Wqa", "wqa", { desc = "Write & quit all files" })
