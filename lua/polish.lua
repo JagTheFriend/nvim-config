8402d79001a4bb4888766b8e9b39a6db2b901cfc
JagTheFriend <JagTheFriend12@gmail.com>
2026-04-01 16:51:56 +0530

    Render whitespaces

--- lua/polish.lua
+++ lua/polish.lua
@@ -8,3 +8,17 @@ vim.api.nvim_create_user_command("W", "w", { desc = "Write file" })
 vim.api.nvim_create_user_command("Wa", "wa", { desc = "Write all files" })
 vim.api.nvim_create_user_command("Wqa", "wqa", { desc = "Write & quit all files" })
 vim.api.nvim_create_user_command("Qa", "qa", { desc = "Quit all files" })
+
+vim.opt.list = true
+vim.opt.listchars = {
+  space = "·",
+  tab = "→ ",
+  trail = "•",
+  eol = "¬",
+}
+
+-- vim.opt.listchars = {
+-- eol = "↴",
+--   trail = "~",
+--   space = "·",
+-- }
