597b7644702ed4184edc52ca7e722b1755ab7204
JagTheFriend <JagTheFriend12@gmail.com>
2026-04-07 21:10:44 +0530

    Setup telescope

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -8,4 +8,13 @@ return {
     "AckslD/muren.nvim",
     config = true,
   },
+  {
+    "nvim-telescope/telescope.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      -- optional but recommended
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+  },
 }
