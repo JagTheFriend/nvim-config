65bf40d034b1f15f9defbb4a8fb9a7bfee1d4996
JagTheFriend <JagTheFriend12@gmail.com>
2026-04-09 15:18:37 +0530

    Fixed bug

--- lua/polish.lua
+++ lua/polish.lua
@@ -31,9 +31,3 @@ vim.api.nvim_set_hl(0, "SpellBad", {
   fg = "skyblue",
   underdashed = true,
 })
-
-local builtin = require "telescope.builtin"
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
