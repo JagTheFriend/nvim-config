0c9bb4bbba73bbc30aaf49f7266e7419e5a0405f
JagTheFriend <JagTheFriend12@gmail.com>
2026-04-09 14:18:25 +0530

    Setup config from astro packs

--- lua/community.lua
+++ lua/community.lua
@@ -1,19 +1,44 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
--- AstroCommunity: import any community modules here
--- We import this file in `lazy_setup.lua` before the `plugins/` folder.
--- This guarantees that the specs are processed before any user plugins.
-
 ---@type LazySpec
 return {
+  -- Core AstroCommunity
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.motion.mini-surround" },
+
+  -- UI / Appearance
   { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   { import = "astrocommunity.recipes.heirline-vscode-winbar" },
+
+  -- Motion / Editing
+  { import = "astrocommunity.motion.mini-surround" },
+
+  -- Language Packs
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.prisma" },
+  { import = "astrocommunity.pack.proto" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.xml" },
+  { import = "astrocommunity.pack.yaml" },
+
+  -- Web / Frontend
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.html-css" },
+
+  -- DevOps / Infra
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.nginx" },
+  { import = "astrocommunity.pack.terraform" },
+
+  -- Linting / Formatting
   { import = "astrocommunity.pack.golangci-lint" },
+  { import = "astrocommunity.pack.biome" },
+  { import = "astrocommunity.pack.oxlint" },
+
+  -- Optional (commented)
   -- { import = "astrocommunity.recipes.astrolsp-no-insert-inlay-hints" },
   -- { import = "astrocommunity.scrolling.mini-animate" },
-  -- import/override with your plugins folder
 }
