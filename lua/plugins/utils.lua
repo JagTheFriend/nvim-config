8d5f50ea7f2b2372cd221e0692aa0d856d459504
JagTheFriend <JagTheFriend12@gmail.com>
2026-04-09 15:11:19 +0530

    Use builtin file finder

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -8,13 +8,4 @@ return {
     "AckslD/muren.nvim",
     config = true,
   },
-  {
-    "nvim-telescope/telescope.nvim",
-    version = "*",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      -- optional but recommended
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    },
-  },
 }
