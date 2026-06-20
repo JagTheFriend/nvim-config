7f548b6c629fc243e247a47cb2a9be098463f0cf
JagTheFriend <JagTheFriend12@gmail.com>
2026-06-20 10:31:13 +0530

    Added custom keybindings to polish.lua

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,33 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+vim.api.nvim_create_user_command("W", "w", { desc = "Write file" })
+vim.api.nvim_create_user_command("Wa", "wa", { desc = "Write all files" })
+vim.api.nvim_create_user_command("Wqa", "wqa", { desc = "Write & quit all files" })
+vim.api.nvim_create_user_command("Qa", "qa", { desc = "Quit all files" })
+
+vim.opt.list = true
+vim.opt.listchars = {
+  space = "·",
+  tab = "→ ",
+  trail = "•",
+  eol = "¬",
+}
+
+-- vim.opt.listchars = {
+-- eol = "↴",
+--   trail = "~",
+--   space = "·",
+-- }
+
+-- Enable spell
+vim.opt.spell = true
+vim.opt.spelllang = { "en_us" }
+
+vim.api.nvim_set_hl(0, "SpellBad", {
+  fg = "skyblue",
+  underdashed = true,
+})
